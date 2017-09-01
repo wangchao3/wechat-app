c13809d4a24df41dc406133658dadf02bf645ee0
wangchao <wangchao@iafclub.com>
2017-09-01 09:16:48 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # wechat-app
-小程序
+
+微信小程序 demo
+
+
+#### 功能及使用的组件等
+* 列表式渲染数据
+* 自定义顶部 tabbar
+* 下拉刷新
+* 上拉加载更多
+* 轮播图
+<br/>等...
+
+### Setup
+
+下载安装Wechat DEV Tools, 并导入项目
+
+### 演示
+
+首页下拉刷新等
+
+![](images/index_scroll.gif)
+
+底部 tab 切换
+
+![](images/bottom_tab.gif)
+
+顶部自定义 tab 切换
+
+![](images/top_tab.gif)
+
+页面跳转
+
+![](images/navigation.gif)
+
+
+##### Demo 用于学习交流, 转载请注明出处
---
+++ pages/list/list.js
@@ -0,0 +1,101 @@
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        navTab: ["散标投资", "债权转让"],
+        currentNavtab: "0",
+        items: [],
+        count: 1
+    },
+
+    onLoad: function () {
+        this.getData();
+    },
+
+    switchTab: function(e) {
+        console.log(e.currentTarget.dataset.idx);
+        this.setData({
+            currentNavtab: e.currentTarget.dataset.idx
+        });
+        this.getData(e.currentTarget.dataset.idx, 1, 10);
+    },
+
+    getData: function(number, pageNo, pageSize) {
+        let currPage = 1;
+        if(pageNo) currPage = pageNo;
+        let pageMany = 10;
+        if(pageSize) pageMany = pageSize;
+        let list_api = 'https://m.iafclub.com/jrrest/investments?pageNo='+currPage+'&pageSize='+pageMany;
+        if (number === 1) {
+            list_api = 'https://m.iafclub.com/jrrest/assignments?pageNo='+currPage+'&pageSize='+pageMany;
+        }
+        let that = this;
+        util.getData(list_api).then(function(res) {
+            console.log(res.data.content);
+            that.setData({
+                items: res.data.content.array ? res.data.content.array : res.data.content.assignmentList,
+            })
+        });
+    },
+
+    upper: function() {
+        wx.showNavigationBarLoading()
+        this.refresh();
+        console.log("upper");
+        setTimeout(function() {
+            wx.hideNavigationBarLoading();
+            wx.stopPullDownRefresh();
+        }, 2000);
+    },
+    lower: function(e) {
+        wx.showNavigationBarLoading();
+        var that = this;
+        setTimeout(function() {
+            wx.hideNavigationBarLoading();
+            that.nextLoad();
+        }, 1000);
+        console.log("lower")
+    },
+
+    refresh: function() {
+        wx.showToast({
+            title: '刷新中',
+            icon: 'loading',
+            duration: 3000
+        });
+
+        console.log("loaddata");
+        this.setData({
+            items: [],
+        });
+        this.getData(this.data.currentNavtab, 1, 10);
+        setTimeout(function() {
+            wx.showToast({
+                title: '刷新成功',
+                icon: 'success',
+                duration: 2000
+            })
+        }, 3000)
+
+    },
+
+    nextLoad: function() {
+        wx.showToast({
+            title: '加载中',
+            icon: 'loading',
+            duration: 4000
+        });
+        this.data.count++;
+        console.log("continueload");
+        console.log(this.data.count);
+        this.getData(this.data.currentNavtab, this.data.count, 10);
+
+        setTimeout(function() {
+            wx.showToast({
+                title: '加载成功',
+                icon: 'success',
+                duration: 2000
+            })
+        }, 3000);
+    }
+})
