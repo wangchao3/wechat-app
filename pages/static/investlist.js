fb3a153c7cbd392acb77777df78c593daffe9e43
wangchao <wangchao@iafclub.com>
2017-09-12 11:37:21 +0800

    update investlist page

--- pages/static/investlist.js
+++ pages/static/investlist.js
@@ -15,7 +15,10 @@ Page({
         let that = this;
         util.getData(detail_api).then(function(res) {
             console.log(res.data.content);
-            res.data.content.projectInfo.endTime = util.formatTime(res.data.content.projectInfo.endTime);
+            res.data.content.userInvestmentList.map((item, index) => {
+                res.data.content.userInvestmentList[index].investTime = util.formatAllTime(res.data.content.userInvestmentList[index].investTime);
+                res.data.content.userInvestmentList[index].investAmt = res.data.content.userInvestmentList[index].investAmt/100;
+            });
             that.setData({
                 info: res.data.content,
             });
--- utils/util.js
+++ utils/util.js
@@ -10,13 +10,25 @@ function formatTime(time) {
     return [year, month, day].map(formatNumber).join('/');
 }
 
+function formatAllTime(time) {
+    var date = new Date(time);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':');
+}
+
 function formatNumber(n) {
     n = n.toString();
     return n[1] ? n : '0' + n;
 }
 
 module.exports = {
-    formatTime: formatTime
+    formatTime: formatTime,
+    formatAllTime: formatAllTime
 };
 
 var index = require('../data/data_index.js')
